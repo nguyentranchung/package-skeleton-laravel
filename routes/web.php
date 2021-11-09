27a50d453f93df21fb58848f45f20952ae507d3a
Nguyễn Trần Chung <nguyentranchung52th@gmail.com>
2021-11-09 17:03:51 +0900

    namespace

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use VendorName\Skeleton\Http\Controllers\IndexController;
+
 Route::group([
     'prefix' => 'xxx',
     'as' => 'xxx.',
     'middleware' => ['web'],
 ], function () {
-    Route::get('/', 'IndexController@index');
+    Route::get('/', [IndexController::class, 'index']);
 });
--- src/Http/Controllers/IndexController.php
+++ src/Http/Controllers/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace FlashVps\Docs\Http\Controllers;
+namespace VendorName\Skeleton\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
